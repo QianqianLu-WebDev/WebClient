10ac96efba3f8e765bb9e2743bdf88c1e4119468
Amoyensis <amoyensis@outlook.com>
2021-07-11 14:25:43 +0800

    ✨ 引入bootstrap、primeng、animate.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebClient';
+  items = [
+    {
+        label: 'File',
+        items: [{
+                label: 'New',
+                icon: 'pi pi-fw pi-plus',
+                items: [
+                    {label: 'Project'},
+                    {label: 'Other'},
+                ]
+            },
+            {label: 'Open'},
+            {label: 'Quit'}
+        ]
+    },
+    {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+            {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+            {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+        ]
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MenuModule} from 'primeng/menu';
+import {MenubarModule} from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+
+import { InputTextModule } from 'primeng/inputtext';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MenubarModule,
+    MenuModule,
+    ButtonModule,
+    InputTextModule,
     BrowserModule,
     AppRoutingModule
   ],
